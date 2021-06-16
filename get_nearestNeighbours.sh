e84a7555e5125f612239a513958c1e80bd404ac7
Sufyan M. Shaikh <63094929+sufyanshk@users.noreply.github.com>
2021-06-16 19:17:12 +0530

    major correction

--- get_nearestNeighbours.sh
+++ get_nearestNeighbours.sh
@@ -2,19 +2,23 @@
 #This needs my_pymatgen to be activated before running
 
 
-#Execute the program as ./get_allNearestNeighbours.sh <RDF first peak>
+#Execute the program as ./get_allNearestNeighbours.sh <lattice parameter from bulk relaxation>
 
-lp=$1 #Here the $1 is the <RDF first peak>
-lp=$(( lp * 1.10 )) #The RDF first peak is further extended by 10%
+lp=$1 #Here the $1 is user inputted lattice parameter found out from bulk relaxation
 
 python ~/Documents/projects/vasputil/scripts/vasputil_nearestneighbors -n 63 CONTCAR > all63NearestNeighbours.txt
 
-awk '!a[$3]++' all63NearestNeighbours.txt > uniques.txt
+awk -v CONVFMT=%.6g '!a[$3]++' all63NearestNeighbours.txt > uniques.txt
 
-awk -v lp="$lp" '{if (NR<=2) print $0; else if ($3<lp) print $0}' uniques.txt > all63NearestNeighbours.txt
+#Here the first nearest neighbor distance is further increased by 10% to accommodate the deviations
+awk -v CONVFMT=%.6f -v lp="$lp" '{if (NR<=2) print $0; else if ($3<(lp*1.10*sqrt(3)/2)) print $0}' uniques.txt > all63NearestNeighbours.txt
 rm uniques.txt 
 
-awk -F '    ' '{if (NR<=2) print $0; else print $1,$2,$3,$3*$3}' all63NearestNeighbours.txt > tmp
+awk -v CONVFMT=%.6g -F '    ' -v lp="$lp" '{if (NR<=2) print $0; else print $1,$2,$3,($3-(lp*sqrt(3)/2)),($3-(lp*sqrt(3)/2))^2}' all63NearestNeighbours.txt > tmp
 
-awk '{for (i=1;i<=NF;i++) sum[i]+=$i} END{print sum[3]/(NR-2)"  <--Average pair distance"; print sqrt(sum[4]/(NR-2))"  <--Root mean squared pair distance"}' tmp 
-rm tmp
+awk -v CONVFMT=%.6g -v lp="$lp" -v sum="0" '{sum=sum+$4*$4} END{print lp*sqrt(3)/2"  <--Ideal first NN distance"; print sqrt(sum/(NR-2))"  <--Root mean squared error"}' tmp > tmp2
+
+cat tmp tmp2 > all63NearestNeighbours.txt
+rm tmp tmp2
+
+tail -2 all63NearestNeighbours.txt
